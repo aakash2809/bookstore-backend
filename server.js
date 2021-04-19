566791b81bdc03bb8d2528dfc7b2098cc22a21c7
aakash2809 <aakashrajak289@gmail.com>
2021-04-20 04:16:52 +0530

    refactor : invoke route

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require("dotenv").config();
 require("./config/index").set(process.env.NODE_ENV, app);
 const config = require("./config/index").get();
 const cors = require('cors');
-const swaggerDocument = require('./app/lib/swagger.json');
+const userRoute = require('./app/routes/user');
+//const swaggerDocument = require('./app/lib/swagger.json');
 
 app.use(cors());
 
@@ -15,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 // parse requests of content-type - application/json 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // listen for request
 app.listen(config.port, () => {
@@ -24,4 +25,6 @@ app.listen(config.port, () => {
 
 new dbconnection(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }).connect();
 
+//Initialize the route
+userRoute.routeToUserController(app);
 module.exports = app;
