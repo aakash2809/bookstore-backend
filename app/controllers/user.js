5475fb36cb4d498459bacedae1a2aa7bc071f975
aakash2809 <aakashrajak289@gmail.com>
2021-04-20 03:09:45 +0530

    refactor : create function to to take rew and send res for register

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -4,8 +4,69 @@
  *               Controller resolve the error using the service layer by invoking its services
  * @author       Aakash Rajak <aakashrajak2809@gmail.com>      
 -----------------------------------------------------------------------------------------------*/
+const logger = require("../../config/logger");
+const userServices = require("../services/user");
+const userValidator = require("../middlewares/userValidator");
+const resposnsCode = require("../../util/staticFile.json");
 
 class UserControllers {
+
+    /**
+   * @description add user to database
+   * @param {*} request in json formate
+   * @param {*} response sends response from server
+   */
+    register = (request, response) => {
+        logger.info(`TRACKED_PATH: Inside controller`);
+        let validatedRequestResult = userValidator.validate(request.body);
+        if (validatedRequestResult.error) {
+            logger.error(`SCHEMAERROR: Request did not match with schema`);
+            response.send({
+                success: false,
+                status_code: resposnsCode.BAD_REQUEST,
+                message: validatedRequestResult.error.details[0].message,
+            });
+            return;
+        }
+
+        const registrationDetails = {
+            name: request.body.name,
+            lastName: request.body.lastName,
+            email: request.body.email,
+            password: request.body.password,
+            confirmPassword: request.body.confirmPassword,
+        };
+
+        if (request.body.password != request.body.confirmPassword) {
+            response.send({
+                success: false,
+                status_code: resposnsCode.BAD_REQUEST,
+                message: "password does not match with confirm password",
+            });
+            return;
+        }
+
+        logger.info(`INVOKING: registerUser method of services`);
+        userServices.registerUser(
+            registrationDetails,
+            (error, registrationResult) => {
+                error
+                    ? response.send({
+                        success: false,
+                        status_code: resposnsCode.BAD_REQUEST,
+                        message: error,
+                    })
+                    : response.send({
+                        success: registrationResult.success,
+                        status_code: registrationResult.statusCode,
+                        message: registrationResult.message,
+                        data: registrationResult.data,
+                    });
+                logger.info("SUCCESS001: User registered successfully");
+            }
+        );
+    };
+
 
 }
 
