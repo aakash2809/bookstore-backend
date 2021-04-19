1b0468ef95e6ad46c2b6d4162f5aa7b16afd445c
aakash2809 <aakashrajak289@gmail.com>
2021-04-20 03:25:47 +0530

    refactor : create schema and function to register

--- app/models/user.js
+++ app/models/user.js
@@ -20,3 +20,85 @@ class UserModel {
 }
 
 module.exports = new UserModel;
+/**
+ * @module       models
+ * @file         userModel.js
+ * @description  This module is used for creating the schema and comunicate with mongodb
+ *               through mongoose
+ * @requires     {@link http://mongoosejs.com/|mongoose} 
+ * @requires     bcryptjs module for encryption of password
+ * @requires     logger is a reference to save logs in log files
+ * @author       Aakash Rajak <aakashrajak2809@gmail.com>
+------------------------------------------------------------------------------------------*/
+
+const mongoose = require(`mongoose`);
+const logger = require("../../config/logger");
+const bycrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        default: null
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    confirmPassword: {
+        type: String,
+    },
+    role: {
+        type: String,
+        default: ""
+    }
+},
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.set('versionKey', false);
+
+userSchema.pre("save", async function (next) {
+    this.password = await bycrypt.hash(this.password, 10);
+    this.confirmPassword = undefined;
+    next();
+})
+
+logger.info('inside model');
+const User = mongoose.model(`User`, userSchema);
+
+class UserModel {
+
+    /**
+      * @description save request data to database 
+      * @param {*} registrationData holds data to be saved in json formate
+      * @param {*} callback holds a function 
+     */
+    register = (registrationData, callback) => {
+        logger.info(`TRACKED_PATH: Inside model`);
+        const userRegistration = new User(registrationData);
+        userRegistration.save((error, registrationResult) => {
+            if (error) {
+                error = "already registered"
+                callback(error, null)
+            } else {
+                callback(null, registrationResult)
+            }
+        })
+    }
+
+
+}
+
+module.exports = new UserModel;
