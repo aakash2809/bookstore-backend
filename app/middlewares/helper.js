53036b6166b7264f0b42d09382b5a3567c87ba8e
aakash2809 <aakashrajak289@gmail.com>
2021-04-20 04:18:54 +0530

    refactor : create Helper class and export it

--- app/middlewares/helper.js
+++ app/middlewares/helper.js
@@ -1,12 +1,17 @@
-/**
-    * @description verify the user to authorized user's role
-    * @param {*} req
-    * @param {*} res
-    * @param {*} next
-    */
-addRole = (role) => {
-    return (req, res, next) => {
-        req.role = role;
-        next();
-    }
-};
+
+class Helper {
+    /**
+        * @description verify the user to authorized user's role
+        * @param {*} req
+        * @param {*} res
+        * @param {*} next
+        */
+    addRole = (role) => {
+        return (req, res, next) => {
+            req.role = role;
+            next();
+        }
+    };
+}
+
+module.exports = new Helper();
