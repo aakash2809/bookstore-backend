fe885b7cd206e73558d24dbe9cb3c3354147bfd0
aakash2809 <aakashrajak289@gmail.com>
2021-04-21 10:07:59 +0530

    refactor : add function to genrate token

--- app/middlewares/helper.js
+++ app/middlewares/helper.js
@@ -1,16 +1,34 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 class Helper {
     /**
-        * @description verify the user to authorized user's role
-        * @param {*} req
-        * @param {*} res
-        * @param {*} next
-        */
+      * @description verify the user to authorized user's role
+      * @param {*} req
+      * @param {*} res
+      * @param {*} next
+      */
     addRole = (role) => {
         return (req, res, next) => {
             req.role = role;
             next();
         }
+    };
+
+    /**
+     * @description it genrate the token
+     */
+    genrateToken = (user) => {
+        return jwt.sign(
+            {
+                role: user.role,
+                userId: user._id,
+            },
+            process.env.SECRET_KEY,
+            {
+                expiresIn: "24h",
+            }
+        );
     };
 }
 
