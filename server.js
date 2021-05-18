95ae75751cfb90c8d8a87eb157fe449a8a17856f
aakash2809 <aakashrajak289@gmail.com>
2021-05-18 10:00:11 +0530

    remove replace console with logger

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const userRoute = require('./app/routes/user');
 const bookRoute = require('./app/routes/book');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./app/lib/swagger.json');
-var multer = require('multer');
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' });
+const logger = require("./config/logger");
 
 app.use(cors());
 
@@ -19,11 +21,13 @@ app.use(express.urlencoded({ extended: true }));
 // parse requests of content-type - application/json 
 app.use(express.json());
 
+
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // listen for request
 app.listen(config.port, () => {
-  console.log(`CONNECT_SERVER: Connected, server started listening on port : ${config.port}`);
+  logger.info(`CONNECT_SERVER: Connected, server started listening on port : ${config.port}`);
 });
 
 new dbconnection(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).connect();
