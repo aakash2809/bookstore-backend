8c38f0dfced13b9426d1da664bdafa7a6b8b13dc
aakash2809 <aakashrajak289@gmail.com>
2021-05-30 20:18:25 +0530

    refactor : use dotenv for google strategy config

--- config/passport.config.js
+++ config/passport.config.js
@@ -3,8 +3,8 @@ const { Strategy } = require("passport-google-oauth20");
 require("dotenv").config();
 
 passport.use(new Strategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_ACCOUNT_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_ACCOUNT_CLIENT_SECRET,
     callbackURL: 'http://localhost:4000/auth/google/callback'
 },
     function (accessToken, refreshToken, profile, done) {
