2ed37bf3844234bda65eef58afcdf99d67859677
aakash2809 <aakashrajak289@gmail.com>
2021-07-09 05:20:48 +0530

    refactor: replace consoles by loggers and resolve eslint errors

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const { Server } = require('socket.io');
+const logger = require('./config/logger');
 
+const io = new Server();
 const app = express();
 const baseUrl = 'http://localhost:2000';
 app.use(cors());
 app.use(express.static(__dirname + '/public/'));
 
 // listen for request
-const server = app.listen(4000, () => {
-    console.log(`Connected, server started listening on port :`, 4000);
+app.listen(4000, () => {
+    logger.info('Connected, server started listening on port :', 4000);
 });
 
-const io = require('socket.io')(server);
-
-/* function getData(payload) {
-    console.log('axios', payload);
-    return axios.post(`${baseUrl}/books/filter/byRange`, payload);
-} */
 let getData = (payload) => {
-    console.log('axios', payload);
     return axios.post(`${baseUrl}/books/filter/byRange`, payload);
 };
 
 io.on('connection', (socket) => {
-    console.log('connected with soket');
+    logger.info('connected with soket');
     socket.on('range', async (payload) => {
         let result = [];
         result = await getData(payload);
-        console.log('result', result.data.data);
         socket.emit('range', result.data.data);
     });
 });
-
