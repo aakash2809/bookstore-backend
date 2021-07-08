9054645f3225adcd6e213e7e1816decb7133bf6d
aakash2809 <aakashrajak289@gmail.com>
2021-07-08 17:44:35 +0530

    refactor: create a function to create graph by dynamic data

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-let data1 = [];
+let data = [];
 const socket = io('http://localhost:4000');
 
 let payload = {
@@ -29,62 +29,56 @@ let payload = {
 socket.emit('range', payload);
 
 socket.on('range', (payload) => {
-    data1 = payload;
-    console.log('client', data1);
+    data = payload;
+    createGraph();
+    console.log('client', payload);
 });
 
-const data = [
-    { range: '0-100', numberOfBooks: 3 },
-    { range: '101-200', numberOfBooks: 3 },
-    { range: '201-300', numberOfBooks: 6 },
-    { range: '301-500', numberOfBooks: 9 },
-    { range: '501-1000', numberOfBooks: 14 },
-];
+function createGraph() {
+    const width = 900;
+    const height = 450;
+    const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-const width = 900;
-const height = 450;
-const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+    const svg = d3.select('#d3-container')
+        .append('svg')
+        .attr('width', width - margin.left - margin.right)
+        .attr('height', height - margin.top - margin.bottom)
+        .attr('viewBox', [0, 0, width, height]);
 
-const svg = d3.select('#d3-container')
-    .append('svg')
-    .attr('width', width - margin.left - margin.right)
-    .attr('height', height - margin.top - margin.bottom)
-    .attr('viewBox', [0, 0, width, height]);
+    const x = d3.scaleBand()
+        .domain(d3.range(data.length))
+        .range([margin.left, width - margin.right])
+        .padding(0.1);
 
-const x = d3.scaleBand()
-    .domain(d3.range(data.length))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
+    const y = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height - margin.bottom, margin.top]);
 
-const y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([height - margin.bottom, margin.top]);
+    svg
+        .append('g')
+        .attr('fill', 'royalblue')
+        .selectAll('rect')
+        .data(data)
+        .join('rect')
+        .attr('x', (d, i) => x(i))
+        .attr('y', d => y(d.numberOfBooks))
+        .attr('title', (d) => d.numberOfBooks)
+        .attr('class', 'rect')
+        .attr('height', d => y(0) - y(d.numberOfBooks))
+        .attr('width', x.bandwidth());
 
-svg
-    .append('g')
-    .attr('fill', 'royalblue')
-    .selectAll('rect')
-    .data(data.sort((a, b) => d3.descending(a.numberOfBooks, b.numberOfBooks)))
-    .join('rect')
-    .attr('x', (d, i) => x(i))
-    .attr('y', d => y(d.numberOfBooks))
-    .attr('title', (d) => d.numberOfBooks)
-    .attr('class', 'rect')
-    .attr('height', d => y(0) - y(d.numberOfBooks))
-    .attr('width', x.bandwidth());
+    function yAxis(g) {
+        g.attr('transform', `translate(${margin.left}, 0)`)
+            .call(d3.axisLeft(y).ticks(null, data.format))
+            .attr('font-size', '20px');
+    }
 
-function yAxis(g) {
-    g.attr('transform', `translate(${margin.left}, 0)`)
-        .call(d3.axisLeft(y).ticks(null, data.format))
-        .attr('font-size', '20px');
+    function xAxis(g) {
+        g.attr('transform', `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x).tickFormat(i => data[i].range))
+            .attr('font-size', '20px');
+    }
+    svg.append('g').call(xAxis);
+    svg.append('g').call(yAxis);
+    svg.node();
 }
-
-function xAxis(g) {
-    g.attr('transform', `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).tickFormat(i => data[i].range))
-        .attr('font-size', '20px');
-}
-
-svg.append('g').call(xAxis);
-svg.append('g').call(yAxis);
-svg.node();
